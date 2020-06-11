f0f2dcd351fc5422e8677a6fadf54d0bf9e35623
Farhaduneci <farhaduneci@gmail.com>
2020-06-11 13:45:28 +0430

    Style complete

--- route/index.js
+++ route/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const authed = require('../middleware/authed');
 
-router.get('/', authed, (req, res) => res.render('index.ejs', { name: req.user.name }));
+router.get('/', authed, (req, res) => res.render('index.ejs', { user: req.user }));
 
 module.exports = router;
